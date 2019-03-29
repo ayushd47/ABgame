51dcdfed4a4573cf6d23e9168a8e8729bd16df79
Ayush <random@gmail.com>
2019-03-29 08:37:21 +0545

    Initial commit

---
+++ app/src/main/java/com/example/ab_game/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ab_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        calculate asd = new calculate();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/ab_game/calculate.java
@@ -0,0 +1,5 @@
+package com.example.ab_game;
+
+public class calculate {
+    int x = 5;
+}
